1ca1a2749c2ecc64862d62e28e66f30bfae7a647
ProfJordan <Jordan.Halliday@slcc.edu>
2023-12-08 14:12:31 -0700

    /api/books .get, .post, & models setup; database connected

--- routes/api.js
+++ routes/api.js
@@ -8,17 +8,50 @@
 
 'use strict';
 
+const Book = require('../models').Book;
+
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
+    .get(async (_req, res) => {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      try {
+      const books = await Book.find({});
+      if (!books) {
+        res.json([]);
+        return;
+      }
+      const formattedBooks = books.map((book) => {
+        return {
+          _id: book._id,
+          title: book.title,
+          comments: book.comments,
+          commentCount: book.comments.length,
+        };
+      });
+      res.json(formattedBooks);
+      return;
+    } catch (err) {
+      res.json([]);
+    }
     })
+
     
-    .post(function (req, res){
-      let title = req.body.title;
+    .post(async (req, res) => {
       //response will contain new book object including atleast _id and title
+      let title = req.body.title;
+      if (!title) {
+        res.send('missing title');
+        return;
+      }
+      const newBook = new Book({title, comments: []});
+      try {
+       const book = await newBook.save();
+        res.json({_id: book._id, title: book.title});
+      } catch (err) {
+        res.send('error saving new book');
+      }
     })
     
     .delete(function(req, res){
